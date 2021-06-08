73bb1b7853d3bfebbfa94616e0cae0801d598224
Jiovanni Romo <srr190802@gmail.com>
2021-06-08 16:50:45 -0500

    Se agrego el dark mode. TODO: Fix preventDefault on the form

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const formTodo = document.querySelector('#formulario');
+const formTodo = document.querySelector('#form-input');
 const mainContainer = document.querySelector('.main-container__todos--items');
 const allBtn = document.querySelector('#all--todos');
 const activeBtn = document.querySelector('#active--todos');
 const completedBtn = document.querySelector('#completed--todos');
 const themeBtn = document.querySelector('.icon');
+const containerFilter = document.querySelector('.item-filter');
 const body = document.querySelector('body');
+const clearBtn = document.querySelector('#clear--todos');
 let todos = [];
-let todosFiltrados = [];
+let filterTODOS = [];
 
 
 //Listeners 
@@ -18,8 +20,6 @@ function loadListeners() {
         todos = JSON.parse(localStorage.getItem('TODOS')) || [];
         console.log(todos)
 
-        const item = document.querySelectorAll('.item--todo');
-
         loadHTML(todos);
     });
 
@@ -28,6 +28,8 @@ function loadListeners() {
     activeBtn.addEventListener('click', activeFilter);
 
     completedBtn.addEventListener('click', completedFilter);
+
+    clearBtn.addEventListener('click', clearCompleted);
 
     themeBtn.addEventListener('click', changeTheme);
 }
@@ -67,11 +69,10 @@ function loadHTML(todosArr = todos) {
             container.setAttribute('id', 'item');
 
             const completed = document.createElement('a');
-            completed.textContent = 'X'
-            completed.classList.add('todo--completed');
-            completed.addEventListener('click', () => {
-                completeTodo(todo.id);
-            });
+            completed.classList.add('complete--todo');
+            completed.onclick = () => {
+                completeTodo(todo.id)
+            }
 
             const paragraphTodo = document.createElement('p');
             paragraphTodo.textContent = todo.todo;
@@ -102,17 +103,20 @@ function cleanHTML() {
 
 function completeTodo(id) {
     let todosUpdated = todos.map(todo => {
+
         if(todo.id === id) {
+
+
             if(todo.complete) {
                 todo.complete = false;
-                syncStorage()
+                syncStorage();
                 return todo;
             } else {
                 todo.complete = true;
-                syncStorage()
-                return todo
+                syncStorage();
+
+                return todo;
             }
-            return todo;
         } else {
             return todo;
         }
@@ -124,15 +128,14 @@ function completeTodo(id) {
 
 function changeTheme() {
 
-    const item = document.querySelector('.item');
-    console.log(item);
-
     if (body.classList.contains('dark')) {
         body.classList.remove('dark');
         body.classList.add('light');
-        formTodo.classList.add('form--light');
+        formTodo.classList.add('light-input');
+        containerFilter.classList.add('light-filter')
     } else if (body.classList.contains('light')) {
-        formTodo.classList.remove('form--light');
+        formTodo.classList.remove('light-input');
+        containerFilter.classList.remove('light-filter')
         body.classList.remove('light');
         body.classList.add('dark');
         console.log('its light')
@@ -149,21 +152,25 @@ function syncStorage() {
 }
 
 function allTodos() {
-    console.log('Loading all TODOS')
     loadHTML();
 }
 
 function activeFilter() {
-    console.log('Filtrando por activos');
-    todosFiltrados = todos.filter(todo => todo.complete === false);
 
-    loadHTML(todosFiltrados);
+    filterTODOS = todos.filter(todo => todo.complete === false);
+
+    loadHTML(filterTODOS);
 }
 
 function completedFilter() {
-    console.log('Filtrando todos completados');
 
-    todosFiltrados = todos.filter(todo => todo.complete === true);
-    loadHTML(todosFiltrados);
-    console.log(todosFiltrados)
+    filterTODOS = todos.filter(todo => todo.complete === true);
+    loadHTML(filterTODOS);
+    console.log(filterTODOS)
+}
+
+function clearCompleted () {
+    todos = todos.filter(todo => todo.complete !== true);
+    loadHTML();
+    syncStorage();
 }
